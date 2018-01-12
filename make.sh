07ebf34083ecfafc5cd260b1d7d3114b8052bd0f
Rogério Pontes <rogeriop062@gmail.com>
2018-01-12 10:28:28 +0000

    ConcurrentScanCoprocessor service

--- make.sh
+++ make.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-protoc -I=src --java_out=src/main/java src/main/proto/Search.proto 
+protoc -I=src --java_out=src/main/java src/main/proto/Search.proto
+protoc -I=src --java_out=src/main/java src/main/proto/Smpc.proto
